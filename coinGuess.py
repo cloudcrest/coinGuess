01cbd96c7e2710a0a87d1864da5b2fa53db2d1a7
Laura Tanase <laura.a.tanase@gmail.com>
2014-04-10 21:07:31 -0400

    2nd commit. Coin game works; no high score or counting though

--- coinGuess.py
+++ coinGuess.py
@@ -1,3 +1,26 @@
 #! usr/bin/env python
 
-f = 
+import random
+
+f = open("highScore.txt", "r+")
+
+def flip_coin():
+    flip = random.choice(["heads", "tails"])
+    return flip
+
+
+guess = raw_input("Heads or tails?")
+
+result = flip_coin()
+
+while guess == result:
+    print "It is %s. Great! Keep going." % (result)
+    guess = raw_input("Is it heads or tails?")
+    result = flip_coin()
+
+else:
+    print "Nope. It's %s." % (result)
+
+
+
+
