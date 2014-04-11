6612be9eeb46f6feb04df925082451a8c3ba2157
Laura Tanase <laura.a.tanase@gmail.com>
2014-04-10 21:29:18 -0400

    3rd commit; game and user's score works. high score doesnt yet. txt linked to file

--- coinGuess.py
+++ coinGuess.py
@@ -2,12 +2,25 @@
 
 import random
 
-f = open("highScore.txt", "r+")
-
 def flip_coin():
     flip = random.choice(["heads", "tails"])
     return flip
 
+# --------------------- Main program below ---------------------------------------
+
+
+
+f = open("highScore.txt", "r+")
+
+highScore = f.read()
+
+highScore = int(highScore)
+
+score = 0
+
+name = raw_input("Hello, what is your name?")
+
+print "Hello", name, "and welcome to the coin guessing game! The all-time high score is %d, held by NAME HERE." % (highScore)
 
 guess = raw_input("Heads or tails?")
 
@@ -17,9 +30,10 @@ while guess == result:
     print "It is %s. Great! Keep going." % (result)
     guess = raw_input("Is it heads or tails?")
     result = flip_coin()
+    score += 1
 
 else:
-    print "Nope. It's %s." % (result)
+    print "Nope. It's %s. Nice try. Your high score is %d. The all-time high score is %d." % (result, score, highScore)
 
 
 
